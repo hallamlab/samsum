73fc534f71cbfa60298ce131e1520ae01402d2c8
Connor Morgan-Lang <c.morganlang@gmail.com>
2019-12-03 18:15:44 -0800

    Relying on tox for testing

--- tests/test_sam_module.py
+++ tests/test_sam_module.py
@@ -1,15 +1,20 @@
 import pytest
-import samsum
+import os
 import glob
 
 from samsum import testing_utils as utils
-from samsum import _sam_module
 
-# @pytest.fixture()
+
+@pytest.fixture()
+def sam_list():
+    data_path = utils.get_test_data("test-data")
+    filenames = sorted(glob.glob(os.path.join(data_path, "*.sam")))
+    return filenames
 
 
 def test_get_mapped_reads():
+    from samsum import _sam_module
     test_sam = utils.get_test_data('pytest_1.sam')
     assert isinstance(test_sam, str)
     mapping_list = _sam_module.get_mapped_reads(test_sam, False, 0)
-    assert (len(mapping_list) == 8)
+    assert (len(mapping_list) == 14)
