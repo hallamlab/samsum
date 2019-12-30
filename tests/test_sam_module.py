0a1a1d18e5bb9d8f2ddbfb31ef5db10f7dbf2c1f
Connor Morgan-Lang <c.morganlang@gmail.com>
2019-12-29 19:54:00 -0800

    New tests for proportion functions

--- tests/test_sam_module.py
+++ tests/test_sam_module.py
@@ -12,6 +12,29 @@ def sam_list():
     return filenames
 
 
+@pytest.fixture()
+def alignment_dat_example():
+    from samsum import classy
+    ex_instance = classy.AlignmentDat("query_read_name")
+    return ex_instance
+
+
+@pytest.fixture()
+def reference_sequence_example():
+    from samsum import classy as ss_class
+    refseq_ex = ss_class.RefSequence("NODE_1", 200)
+    sam_dat = {"q1": ["NODE_1", "1", "5S45M", "0", "1.0"],
+               "q2": ["NODE_1", "1", "45M", "0", "1.0"],
+               "q3": ["NODE_1", "1", "5S145M", "0", "1.0"],
+               "q4": ["NODE_1", "1", "5S145M", "0", "1.0"]}
+    refseq_ex.reads_mapped = len(sam_dat)
+    for r in sam_dat:
+        aln_dat = ss_class.AlignmentDat(r)
+        aln_dat.load_sam(sam_dat[r])
+        refseq_ex.alignments.append(aln_dat)
+    return refseq_ex
+
+
 def test_get_mapped_reads():
     from samsum import _sam_module
     test_sam = utils.get_test_data('pytest_1.sam')
@@ -19,13 +42,6 @@ def test_get_mapped_reads():
     mapping_list = _sam_module.get_mapped_reads(test_sam, False, 0)
     assert (len(mapping_list) == 16)
     return
-
-
-@pytest.fixture()
-def alignment_dat_example():
-    from samsum import classy
-    ex_instance = classy.AlignmentDat("query_read_name")
-    return ex_instance
 
 
 def test_load_sam(alignment_dat_example):
@@ -66,11 +82,17 @@ def test_ref_sequence_abundances():
     test_sam = utils.get_test_data("samsum_test_2.sam")
     test_aln = utils.get_test_data("samsum_test_2.fasta")
     ref_seq_abunds = commands.ref_sequence_abundances(aln_file=test_sam, seq_file=test_aln,
-                                                      min_aln=10, p_cov=10, map_qual=0)
+                                                      min_aln=10, p_cov=0, map_qual=0)
     e10 = ref_seq_abunds['AB-755_P22_E10_NODE_6_length_36342_cov_2156.57_ID_21']  # type: ss_class.RefSequence
     assert e10.reads_mapped == 10
     assert e10.weight_total == 5.0
     return
+
+
+def test_proportion_covered(reference_sequence_example):
+    assert reference_sequence_example.reads_mapped == 4
+    proportion = reference_sequence_example.proportion_covered()
+    assert proportion == 0.72
 
 # TODO: Write test to ensure the RefSequence.rightmost doesn't exceed its length from alignment_dat_example.load_sam()
 
--- tests/test_samsum.py
+++ tests/test_samsum.py
@@ -1,1 +1,9 @@
-import pytest
+
+def test_overlapping_intervals():
+    from samsum import alignment_utils
+    coords_one = (0, 100)
+    coords_two = (50, 101)
+    coords_three = (101, 200)
+    assert alignment_utils.overlapping_intervals(coords_one, coords_two) is True
+    assert alignment_utils.overlapping_intervals(coords_two, coords_three) is True
+    assert alignment_utils.overlapping_intervals(coords_one, coords_three) is False
