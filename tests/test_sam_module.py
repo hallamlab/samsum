b19fd60db5fb7b97838cf0caed97e62b6e0dd5d9
Matthew <cnimaiv@gmail.com>
2020-06-23 20:02:08 -0700

    updated test for loead_dict

--- tests/test_sam_module.py
+++ tests/test_sam_module.py
@@ -48,7 +48,7 @@ def test_get_mapped_reads():
     test_sam = utils.get_test_data('pytest_1.sam')
     assert isinstance(test_sam, str)
     mapping_list = _sam_module.get_mapped_reads(test_sam, False, 10, 0, 'q')
-    assert (len(mapping_list) == 16)
+    assert (len(mapping_list) == 8)
     return
 
 
