cc8fb1f29299f2f791bcfd99df9ce43f3cee01a4
cmorganl <c.morganlang@gmail.com>
2021-01-05 19:40:56 +0100

    debugging build test

--- src/extensions/sambamparser.cpp
+++ src/extensions/sambamparser.cpp
@@ -129,12 +129,13 @@ bool SamFileParser::nextline(MATCH *match) {
      cout << "Setting query to " << this->fields[0] << endl;
      match->query = (std::string)this->fields[0];
      cout << "Setting subject to " << this->fields[2] << endl;
-     match->subject = (std::string)this->fields[2];
+     match->subject = this->fields[2];
      cout << "Setting start" << endl;
      match->start = atoi(this->fields[3]);
      cout << "Setting mq" << endl;
      match->mq = atoi(this->fields[4]);
-     match->cigar = (std::string)this->fields[5];
+     cout << "Setting cigar to " << this->fields[5] << endl;
+     match->cigar = this->fields[5];
      cout << "Setting paired" << endl;
      match->paired = getMateInfo(static_cast<unsigned int>(atoi(this->fields[1])), match);
      cout << "Testing parity" << endl;
