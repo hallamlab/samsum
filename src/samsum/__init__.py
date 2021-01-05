31811aca396a7ee459fc1fde552e11731a382a18
cmorganl <c.morganlang@gmail.com>
2021-01-05 16:23:26 +0100

    Fix version import

--- src/samsum/__init__.py
+++ src/samsum/__init__.py
@@ -6,5 +6,5 @@ import _sam_module
 from samsum import _version as samsum_version
 
 name = "samsum"
-__version__ = samsum_version
+__version__ = samsum_version.__version__
 __all__ = ["_sam_module"]
